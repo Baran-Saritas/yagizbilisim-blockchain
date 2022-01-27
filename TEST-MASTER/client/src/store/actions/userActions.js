4d797412deb66588234df2e2839467d36167da9c
Baran Sarıtaş <75392592+Baran-Saritas@users.noreply.github.com>
2022-01-27 20:13:44 +0300

    update

--- TEST-MASTER/client/src/store/actions/userActions.js
+++ TEST-MASTER/client/src/store/actions/userActions.js
@@ -12,11 +12,16 @@ export const loginUser = (email,password) => async (dispatch) =>  {
     try {
         dispatch({type:POST_DATA_REQUEST});
 
-        //const response = await axios.get
+        axios.post("http://localhost:8080/users",{email, password}).then((res)=>{console.log("RES: ", res.data);});
 
-        
+        dispatch({
+            type: POST_DATA_SUCCESS,
+          });
     } catch (error) {
-        
+        dispatch({
+            type: POST_DATA_FAILURE,
+            payload: error.data.messages.severityMessageMap.ERROR[0].message,
+          });
     }
 
 
--- TEST-MASTER/client/src/store/reducers/userReducer/index.js
+++ TEST-MASTER/client/src/store/reducers/userReducer/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import {
     GET_DATA_REQUEST,
     GET_DATA_SUCCESS,
