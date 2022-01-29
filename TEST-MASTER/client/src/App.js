4f8c38f1eb91a5dc1eff7fb6ae05e038709b57bf
Ziya <ziyakaragoz42@gmail.com>
2022-01-29 22:36:32 +0300

    SignupScreen frontend bitti

--- TEST-MASTER/client/src/App.js
+++ TEST-MASTER/client/src/App.js
@@ -2,8 +2,11 @@ import "./App.css";
 import "./style.css";
 import Transaction from "./chain/Transaction";
 import BlockChain from "./chain/BlockChain";
-import React, { useState, useEffect,useRef } from "react";
+import LoginScreen from "./components/LoginScreen";
+
+import React, { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
+import SignupScreen from "./components/SignupScreen";
 const EC = require("elliptic").ec;
 const ec = new EC("secp256k1");
 
@@ -21,7 +24,7 @@ function App() {
   const [fileName, setFileName] = useState("");
   const [res, setRes] = useState("");
   const [notify, setNotify] = useState(false);
-  const [code, setCode] = useState("");   // kisinin public keyi
+  const [code, setCode] = useState(""); // kisinin public keyi
   const [belge, setBelge] = useState([]);
 
   const opt1 = useRef(null);
@@ -43,9 +46,7 @@ function App() {
     reader.readAsDataURL(fileName);
   };
 
-
   useEffect(() => {
-
     if (res !== "") {
       console.log("trans blogu");
       let timer = new Date();
@@ -75,23 +76,22 @@ function App() {
     }
   }, [res]);
 
-
-  useEffect(() => {    // kod atamasi
+  useEffect(() => {
+    // kod atamasi
     let res = prompt("kisi kodu girin");
     if (res !== null && res !== "") {
       setCode(res);
     }
   }, []);
 
-
   useEffect(() => {
     if (!code) return;
-    console.log("socket.current :",socket.current);
+    console.log("socket.current :", socket.current);
     socket.current = io("http://localhost:5000/", {
       transports: ["websocket", "polling", "flashsocket"],
     });
     blockChain.current = new BlockChain("null");
-    console.log("socket.current :",socket.current.blockChain);
+    console.log("socket.current :", socket.current.blockChain);
     key.current = ec.keyFromPrivate(
       code + "6abc91f1cd74bcfccc5b0508f6d7e019d114e2e99139a2d11ff362cd6ffc82c"
     );
@@ -116,7 +116,7 @@ function App() {
       console.log("send blogu");
       let timer = new Date();
       blockChain.current.chain = JSON.parse(chain);
-      console.log("x",JSON.parse(chain));
+      console.log("x", JSON.parse(chain));
       let tempArray = blockChain.current.chain.filter((el) => {
         console.log();
         return (
@@ -126,88 +126,92 @@ function App() {
       setBelge(tempArray);
       console.log("send blogu zamani : ", (new Date() - timer) / 1000);
     });
-
   }, [code]);
 
+  // return (
+  //   <div className="App">
+  //     <div className="container">
+  //       <form className="form-inline">
+  //         <label htmlFor="paper-type">Belge Türü</label>
+  //         <select
+  //           defaultValue={"default"}
+  //           onChange={(e) => setBelgeTuru(e.target.value)}
+  //           name="paper-type"
+  //           id="paper-type"
+  //         >
+  //           <option ref={opt1} value="default" disabled hidden>
+  //             Belge Türü Seçiniz...
+  //           </option>
+  //           <option value="obelgesi">Öğrenci Belgesi</option>
+  //           <option value="transkript">Transkript</option>
+  //         </select>
+
+  //         <label htmlFor="kisi">Gönderilecek Kişi</label>
+  //         <select
+  //           defaultValue={"default"}
+  //           onChange={(e) => setKisi(e.target.value)}
+  //           name="kisi"
+  //           id="kisi"
+  //         >
+  //           <option ref={opt2} value="default" disabled hidden>
+  //             Kişi Seçiniz...
+  //           </option>
+  //           {Object.keys(publicKeys).map((el) => {
+  //             if (el !== key?.current?.getPublic("hex")) {
+  //               //console.log("Current : ",key.current.getPublic("hex"));
+  //               return (
+  //                 <option key={el} value={publicKeys[el]}>
+  //                   {
+  //                     { baran: "Baran", ozcan: "Özcan", ziya: "Ziya" }[
+  //                       publicKeys[el]
+  //                     ]
+  //                   }
+  //                 </option>
+  //               );
+  //             }
+  //           })}
+  //         </select>
+
+  //         <label htmlFor="belge-yukleme">Belge Yükle:</label>
+  //         <input
+  //           ref={file}
+  //           onChange={(e) => setFileName(e.target.files[0])}
+  //           type="file"
+  //           name="belge-yukleme"
+  //           accept=".pdf"
+  //           id="belge-yukleme"
+  //         />
+
+  //         <button onClick={send} type="submit">
+  //           Gönder
+  //         </button>
+  //         {notify && (
+  //           <div id="notify">{`${fileName.name} adlı dosya ${kisi} adlı kişiye gönderildi!`}</div>
+  //         )}
+  //       </form>
+
+  //       <div className="right">
+  //         {belge.map((eleman, index) => {
+  //           return (
+  //             <div key={index} className="def">
+  //               <a
+  //                 href={eleman.transactions[0].data.data}
+  //                 target="_self"
+  //                 download={eleman.transactions[0].data.fName + ".pdf"}
+  //               >
+  //                 <p>{eleman.transactions[0].data.fName}</p>
+  //               </a>
+  //               <p>{publicKeys[eleman.transactions[0].sender]}</p>
+  //             </div>
+  //           );
+  //         })}
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <div className="container">
-        <form className="form-inline">
-          <label htmlFor="paper-type">Belge Türü</label>
-          <select
-            defaultValue={"default"}
-            onChange={(e) => setBelgeTuru(e.target.value)}
-            name="paper-type"
-            id="paper-type"
-          >
-            <option ref={opt1} value="default" disabled hidden>
-              Belge Türü Seçiniz...
-            </option>
-            <option value="obelgesi">Öğrenci Belgesi</option>
-            <option value="transkript">Transkript</option>
-          </select>
-
-          <label htmlFor="kisi">Gönderilecek Kişi</label>
-          <select
-            defaultValue={"default"}
-            onChange={(e) => setKisi(e.target.value)}
-            name="kisi"
-            id="kisi"
-          >
-            <option ref={opt2} value="default" disabled hidden>
-              Kişi Seçiniz...
-            </option>
-            {Object.keys(publicKeys).map((el) => {
-              if (el !== key?.current?.getPublic("hex")) {
-                //console.log("Current : ",key.current.getPublic("hex"));
-                return (
-                  <option key={el} value={publicKeys[el]}>
-                    {
-                      { baran: "Baran", ozcan: "Özcan", ziya: "Ziya" }[
-                        publicKeys[el]
-                      ]
-                    }
-                  </option>
-                );
-              }
-            })}
-          </select>
-
-          <label htmlFor="belge-yukleme">Belge Yükle:</label>
-          <input
-            ref={file}
-            onChange={(e) => setFileName(e.target.files[0])}
-            type="file"
-            name="belge-yukleme"
-            accept=".pdf"
-            id="belge-yukleme"
-          />
-
-          <button onClick={send} type="submit">
-            Gönder
-          </button>
-          {notify && (
-            <div id="notify">{`${fileName.name} adlı dosya ${kisi} adlı kişiye gönderildi!`}</div>
-          )}
-        </form>
-
-        <div className="right">
-          {belge.map((eleman, index) => {
-            return (
-              <div key={index} className="def">
-                <a
-                  href={eleman.transactions[0].data.data}
-                  target="_self"
-                  download={eleman.transactions[0].data.fName + ".pdf"}
-                >
-                  <p>{eleman.transactions[0].data.fName}</p>
-                </a>
-                <p>{publicKeys[eleman.transactions[0].sender]}</p>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+    <div>
+      <SignupScreen />;
     </div>
   );
 }
---
+++ TEST-MASTER/client/src/components/SignupScreen.js
@@ -0,0 +1,107 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "../styles/SignupScreen.css";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
+import { emailValidator } from "../helpers/emailValidator";
+import {
+  passwordValidator,
+  passwordControlValidator,
+} from "../helpers/passwordValidator";
+function SignupScreen() {
+  const onFinish = (values) => {
+    if (emailValidator(values.email)) {
+      alert("Incorrect Email!");
+    }
+    if (passwordValidator(values.password)) {
+      alert("Password must be at least 5 characters!");
+    }
+    if (passwordControlValidator(values.password, values.passwordControl)) {
+      alert("Two passwords that you entered are not matching!");
+    }
+    console.log("Formdan Gelen Veriler: ", values);
+  };
+  return (
+    <Form
+      name='normal_signup'
+      className='signup-form'
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name='email'
+        rules={[
+          {
+            required: true,
+            message: "Please input your email address!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<MailOutlined className='site-form-item-icon' />}
+          placeholder='Email'
+        />
+      </Form.Item>
+      <Form.Item
+        name='username'
+        rules={[
+          {
+            required: true,
+            message: "Please input your Username!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className='site-form-item-icon' />}
+          placeholder='Username'
+        />
+      </Form.Item>
+      <Form.Item
+        name='password'
+        rules={[
+          {
+            required: true,
+            message: "Please input your Password!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className='site-form-item-icon' />}
+          type='password'
+          placeholder='Password'
+        />
+      </Form.Item>
+      <Form.Item
+        name='passwordControl'
+        rules={[
+          {
+            required: true,
+            message: "Passwords must match!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className='site-form-item-icon' />}
+          type='password'
+          placeholder='Again Password'
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name='already-have-an-account'>
+          <a href='#' className='login-navigation'>
+            Already have an account?
+          </a>
+        </Form.Item>
+      </Form.Item>
+      <Form.Item>
+        <Button type='primary' htmlType='submit' className='signup-form-button'>
+          Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default SignupScreen;
